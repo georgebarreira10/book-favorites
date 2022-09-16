dcc378dcbf8a24ad6111eb9b4b01b674edb0dbff
george barreira <barreirageorge@gmail.com>
2022-09-15 21:07:16 -0300

    Desenvolvemos a tela de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import './Login.css'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <main className="form-signin w-100 m-auto">
+      <form>
+        <img className="mb-4" src="https://pngimg.com/uploads/bmw/bmw_PNG99548.png" alt="" width="72" height="57"/>
+          <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+          <div className="form-floating">
+            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
+              <label>Email address</label>
+          </div>
+          <div className="form-floating">
+            <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
+              <label>Password</label>
+          </div>
+
+          <div className="checkbox mb-3">
+            <label>
+              <input type="checkbox" value="remember-me"/> Remember me
+            </label>
+          </div>
+          <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+          <p className="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
+      </form>
+    </main>
+
   )
 }
