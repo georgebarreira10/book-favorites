ed703cec0ce1ef5adb9dceac5a6696208c121073
george barreira <barreirageorge@gmail.com>
2022-09-20 20:56:15 -0300

    install react bootstrap table. Axios. Async. Await

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import BootstrapTable from 'react-bootstrap-table-next'
+
 
 export default function Home() {
+
+  const books = [
+    {
+
+      id: 1,
+      nome: "A Ira",
+    }, {
+
+      id: 2,
+      nome: "Dom Casmurro",
+    }
+  ]
+
+  const colunas = [{
+    datafield: 'id',
+    text: 'ID'
+  },
+  {
+    datafield: 'nome',
+    text: 'LIVRO',
+
+  }];
+
+
   return (
-    <h1>Home</h1>
+    <BootstrapTable keyField="id" data={ books } columns={ colunas } />
   )
+
 }
+
